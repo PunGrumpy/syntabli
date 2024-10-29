2cafb0fc168f8e16893268cb11b59eb4baf5b57e
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-29 20:30:51 +0700

    fix(app/page): add promise for compatible `PageProps`

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -3,14 +3,15 @@ import * as React from 'react'
 import { Client } from '@/app/(app)/client'
 import { dataOptions } from '@/app/(app)/query-options'
 import { searchParamsCache } from '@/app/(app)/search-params'
+import { SearchParams } from '@/components/data-table/types'
 import { getQueryClient } from '@/components/query/get-query-client'
 
 interface AppPageProps {
-  searchParams: { [key: string]: string | string[] | undefined }
+  searchParams: Promise<SearchParams>
 }
 
 export default async function AppPage({ searchParams }: AppPageProps) {
-  const search = searchParamsCache.parse(searchParams)
+  const search = await searchParamsCache.parse(searchParams)
   const queryClient = getQueryClient()
   await queryClient.prefetchInfiniteQuery(dataOptions(search))
 
